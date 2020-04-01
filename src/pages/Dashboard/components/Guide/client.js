4579fc1b5ee0ee12d8384cc16657e07cb6999c14
lowkey956 <xinghao.wang@hotmail.com>
2020-04-01 14:55:05 +0800

    主流程跑通,能正常显示和操作远程游戏

---
+++ src/pages/Dashboard/components/Guide/client.js
@@ -0,0 +1,238 @@
+let localStream;
+
+const ioEvent = {
+  mousemove : 11,
+  mousedown : 12,
+  mouseup : 13,
+  keyup : 21,
+  keydown : 22
+}
+
+module.exports = function(jobId) {
+  return new WebsocketClient(jobId);
+};
+
+function WebsocketClient(jId){
+  this.receiveChannel = {};
+  this.pc = {};
+  this.pc1 = {};
+  this.jobId = jId;
+  this.ws = new WebSocket('ws://10.0.19.15:5000');
+
+  this.ws.onopen = function(evt) {  // 绑定连接事件
+    // this.start();
+    console.log('Connection open ...',evt);
+    // ws.send("发送的数据");
+    let login = {
+      msgType : 'login',
+      jobId : jId
+    };
+    login = JSON.stringify(login);
+    this.ws.send(login);
+  }.bind(this);
+  this.ws.onmessage = function(evt) {// 绑定收到消息事件
+    this.HandleMessage(evt.data);
+  }.bind(this);
+  this.ws.onclose = function(evt) { // 绑定关闭或断开连接事件
+    console.log('Connection closed.',evt);
+  };
+}
+
+WebsocketClient.prototype.gotStream = function (stream) {
+  console.log('Received local stream');
+  localStream = stream;
+}
+
+WebsocketClient.prototype.start = function() {
+  console.log('Requesting local stream');
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: true
+    })
+    .then(this.gotStream)
+    .catch(e => alert(`getUserMedia() error: ${e.name}`));
+}
+
+WebsocketClient.prototype.HandleMessage = function(message) {
+  const packeg = JSON.parse(message);
+  this.Dispatch(packeg);
+}
+
+
+WebsocketClient.prototype.Dispatch = function(message) {
+  console.log('[info][client][action:Dispatch][contain:' + message.msgType + ']');
+  switch (message.msgType) {
+    case 'logged':
+      this.HandleLogged(message);
+      break;
+    case 'sdp':
+      this.HandleSDP(message);
+      break;
+    case 'candidate':
+      this.HandleCondidate(message);
+      break;
+    default:
+      break;
+  }
+}
+
+WebsocketClient.prototype.SendMessage = function(message){
+  this.ws.send(message);
+}
+
+window.moz = !!navigator.mozGetUserMedia;
+var chromeVersion = !!navigator.mozGetUserMedia ? 0 : parseInt(navigator.userAgent.match( /Chrom(e|ium)\/([0-9]+)\./ )[2]);
+
+WebsocketClient.prototype.onIceCandidate = function(pc, event) {
+  // 发送给对方event.candidate
+  const msg = {
+    msgType:'candidate',
+    candidate: event.candidate
+  };
+  console.log('[info][client][action:condidate][contain:send condidate]');
+  this.SendMessage(JSON.stringify(msg));
+}
+
+WebsocketClient.prototype.HandleLogged = function(msg) {
+
+  let iceServers = [];
+  if (moz) {
+    iceServers.push({
+      url: 'stun:23.21.150.121'
+    });
+
+    iceServers.push({
+      url: 'stun:stun.services.mozilla.com'
+    });
+  }
+
+  if (!moz) {
+    iceServers.push({
+      url: 'stun:stun.l.google.com:19302'
+    });
+
+    iceServers.push({
+      url: 'stun:stun.anyfirewall.com:3478'
+    });
+  }
+
+  if (!moz && chromeVersion < 28) {
+    iceServers.push({
+      url: 'turn:homeo@turn.bistri.com:80',
+      credential: 'homeo'
+    });
+  }
+
+  if (!moz && chromeVersion >= 28) {
+    iceServers.push({
+      url: 'turn:turn.bistri.com:80',
+      credential: 'homeo',
+      username: 'homeo'
+    });
+
+    iceServers.push({
+      url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+      credential: 'webrtc',
+      username: 'webrtc'
+    });
+  }
+
+  iceServers = {
+    iceServers: iceServers
+  };
+
+  this.pc = new RTCPeerConnection(iceServers);
+  this.pc.addEventListener('icecandidate', function(e){
+    this.onIceCandidate(this.pc, e);
+  }.bind(this));
+  // this.pc.addEventListener('icecandidate', (e) => {this.onIceCandidate(this.pc, e)}.bind(this));
+  this.pc.addEventListener('track', this.gotRemoteStream.bind(this));
+  this.pc.addEventListener('datachannel', this.receiveChannelCallback.bind(this));
+}
+
+WebsocketClient.prototype.HandleSDP = function(message) {
+  this.pc.setRemoteDescription(new RTCSessionDescription(message['sdp'])).then(()=>{
+    console.log('[info][client][action:sdp][contain:set remote sdp.]');
+    this.pc.createAnswer().then((desc)=>{
+      console.log('[info][client][action:sdp][contain:create answer.]');
+      this.pc.setLocalDescription(desc).then(()=>{
+        console.log('[info][client][action:sdp][contain:set local sdp.]');
+        // 发送给对方sdp
+        const msg = {
+          msgType: 'sdp',
+          sdp : desc
+        };
+        this.SendMessage(JSON.stringify(msg))
+      });
+    })
+  });
+
+}
+
+WebsocketClient.prototype.HandleCondidate = function(msg) {
+  console.log('[info][client][action:condidate][contain:set remote condidate.]');
+  const value = msg['candidate'];
+  const candidate = {
+    candidate: value['candidate'],
+    sdpMid:value['sdpMid'],
+    sdpMLineIndex:value['sdpMLineIndex']
+  };
+  this.pc.addIceCandidate(new RTCIceCandidate(candidate));
+}
+
+WebsocketClient.prototype.gotRemoteStream = function(e) {
+  const remoteVideo = document.getElementById('remoteVideo');
+  if (remoteVideo.srcObject !== e.streams[0]) {
+    remoteVideo.srcObject = e.streams[0];
+    console.log('[info][client][action:stream][contain:set remote stream.]');
+  }
+}
+
+WebsocketClient.prototype.receiveChannelCallback = function(event) {
+  const videoContainer = document.getElementById('videoContainer');
+
+  const remoteVideo = document.getElementById('remoteVideo');
+  console.log('Receive Channel Callback');
+  this.receiveChannel = event.channel;
+  this.receiveChannel.binaryType = 'arraybuffer';
+  this.receiveChannel.addEventListener('close', this.onReceiveChannelClosed.bind(this));
+  this.receiveChannel.addEventListener('message', this.onReceiveMessageCallback.bind(this));
+
+  let rect = remoteVideo.getBoundingClientRect();
+  let fpsTime = (new Date()).getTime();
+  videoContainer.addEventListener('mousemove',function (ev) {
+    if (((new Date()).getTime() - fpsTime) < 30)
+      return;
+    let mouseX = ev.clientX-rect.x;
+    let mouseY = ev.clientY-rect.y;
+    this.receiveChannel.send(new Uint32Array([ioEvent.mousemove,mouseX,mouseY]));
+  }.bind(this),false);
+  videoContainer.addEventListener('mousedown',function (ev) {
+    let mouseX = ev.clientX-rect.x;
+    let mouseY = ev.clientY-rect.y;
+    this.receiveChannel.send(new Uint32Array([ioEvent.mousedown,ev.button,mouseX,mouseY]));
+    console.log('mousedown, x = ' + mouseX + 'y = ' + mouseY);
+  }.bind(this),false);
+  videoContainer.addEventListener('mouseup',function (ev) {
+    let mouseX = ev.clientX-rect.x;
+    let mouseY = ev.clientY-rect.y;
+    this.receiveChannel.send(new Uint32Array([ioEvent.mouseup,ev.button,mouseX,mouseY]));
+    console.log('mouseup, x = ' + mouseX + 'y = ' + mouseY);
+  }.bind(this),false);
+  window.addEventListener('keydown',function (ev) {
+    this.receiveChannel.send(new Uint32Array([ioEvent.keydown,ev.keyCode]));
+  }.bind(this),false);
+  window.addEventListener('keyup',function (ev) {
+    this.receiveChannel.send(new Uint32Array([ioEvent.keyup,ev.keyCode]));
+  }.bind(this),false);
+}
+
+WebsocketClient.prototype.onReceiveMessageCallback = function(event) {
+  const message = event.data;
+  console.log(message);
+}
+
+WebsocketClient.prototype.onReceiveChannelClosed = function() {
+  console.log('Receive channel is closed');
+}
--- src/pages/Dashboard/components/Guide/index.jsx
+++ src/pages/Dashboard/components/Guide/index.jsx
@@ -1,53 +1,106 @@
 import * as React from 'react';
 import { Button } from '@alifd/next';
 import styles from './index.module.scss';
+import websocketclient from './client';
 
-let pomelo = window.pomelo;
+const pomelo = window.pomelo;
+pomelo.on('jobMsg',OnMessage);
+pomelo.on('showInClient',OnMessage);
+const returnMsg = {
+  authFail: '验证失败',
+  jobFail: '任务处理失败',
+  jobDispatched: '任务已经派发',
+  jobInit: '已生成任务数据',
+  invalidArgs_jobType: '无效的参数jobType',
+  invalidArgs_expId: '无效的参数expId',
+  maxJob: '超过实验最大请求数',
+  noIdleServer: '无可用服务器',
+}
+const codeMsg = ['authFail', 'jobFail', 'jobDispatched','jobInit','invalidArgs_jobType','invalidArgs_expId','maxJob','noIdleServer','end']
+function OnMessage(data){
+  switch(data.code){
+    case 0:
+      alert(returnMsg[codeMsg[0]]);
+      break;
+    case 1:
+      alert(returnMsg[codeMsg[1]]);
+      break;
+    case 2:
+      alert(returnMsg[codeMsg[2]]);
+      pomelo.rtcClient = websocketclient(data.msg);
+      break;
+    case 3:
+      alert(returnMsg[codeMsg[3]]);
+      break;
+    case 4:
+      alert(returnMsg[codeMsg[4]]);
+      break;
+    case 5:
+      alert(returnMsg[codeMsg[5]]);
+      break;
+    case 6:
+      alert(returnMsg[codeMsg[6]]);
+      break;
+    case 7:
+      alert(returnMsg[codeMsg[7]]);
+      break;
+    default:
+      break;
+  }
+}
 function entry(host, port, callback) {
-      // init socketClient
-      // TODO for development
-      if(host === '127.0.0.1') {
-        host = '10.0.19.29';
+  // init socketClient
+  // TODO for development
+  if(host === '127.0.0.1') {
+    // eslint-disable-next-line no-param-reassign
+    host = '10.0.19.15';
+  }
+  pomelo.init({'host': host, 'port': port, log: true}, function() {
+    pomelo.request('connector.entryHandler.entry', {userName: 'lowkey',password: '123456'}, function(data) {
+      if(!data){
+        alert('获取实验列表为空!');
       }
-      pomelo.init({host: host, port: port, log: true}, function() {
-        pomelo.request('connector.entryHandler.entry', {userName: 'userName',password: '123456'}, function(data) {
-
-          if (callback) {
-            callback(data.code);
-          }
-
-          pomelo.request('jobDispatch.jobDispatchHandler.doJob',{jobType: 'experiment',experimentId: data[0]});
-
-        });
+      if (callback) {
+        callback(data.code);
+      }
+      console.log('获取实验列表成功，将启动实验。');
+      console.log(`data keys is : ${Object.keys(data)}`);
+      console.log(`data values is  : ${Object.values(data)}`);
+      pomelo.request('jobDispatch.jobDispatchHandler.doJob',{jobType: 0,experimentId: data[0]},function(data1) {
+        console.log(`jobDispatch.jobDispatchHandler.doJob is callback. return value is ${  data1}`);
       });
-    }
 
-    function queryEntry(uid, callback) {
-      pomelo.init({host: '10.0.19.29', port: '3040', log: true}, function() {
-        pomelo.request('gate.gateHandler.queryEntry', { uid: uid}, function(data) {
-          pomelo.disconnect();
+    });
+  });
+}
 
-          if(data.code === 2001) {
-            alert('Servers error!');
-            return;
-          }
-          callback(data.host, data.port);
-        });
-      });
-    }
+function queryEntry(uid, callback) {
+  pomelo.init({host: '10.0.19.15', port: '3040', log: true}, function() {
+    pomelo.request('gate.gateHandler.queryEntry', { 'uid': uid}, function(data) {
+      pomelo.disconnect();
+
+      if(data.code === 2001) {
+        // eslint-disable-next-line no-alert
+        alert('Servers error!');
+        return;
+      }
+      callback(data.clientHost, data.clientPort);
+    });
+  });
+}
 
 function authEntry(uid, callback) {
-      queryEntry(uid, function(host, port) {
-        entry(host, port, callback);
-      });
-    }
+  queryEntry(uid, function(host, port) {
+    console.log(`host : ${  host  }, port : ${  port}`);
+    entry(host, port, callback);
+  });
+}
 
 function onStartExperimentClick(){
-  console.log("点击了启动实验按钮");
-  entry('10.0.19.29',3010);
-  // authEntry('userName',(data)=>{
-  // console.log(data.code);
-  // });
+  console.log('点击了启动实验按钮');
+  authEntry('userName',(data)=>{
+    console.log(data);
+  });
 }
 
 const Guide = () => (
@@ -55,7 +108,9 @@ const Guide = () => (
     <h2 className={styles.title}>Welcome to icejs!</h2>
 
     <p className={styles.description}>This is a awesome project, enjoy it!</p>
-
+    <div id="videoContainer">
+      <video id="remoteVideo" playsinline autoPlay muted><track  /></video>
+    </div>
     <div className={styles.action}>
       <Button type="primary" size="large" onClick={onStartExperimentClick}>
         启动实验
